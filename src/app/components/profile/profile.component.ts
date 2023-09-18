6b4a3ffd724b72b579e508805770794fcf8bbe16
Yusuf Safa Koksal <yusuf68815@gmail.com>
2023-09-18 23:41:31 +0300

    Added change password feature

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/services/user.service';
 import { FormGroup,FormBuilder,FormControl,Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -14,7 +17,10 @@ export class ProfileComponent implements OnInit {
   changeNameForm:FormGroup;
 
   constructor(private userService:UserService,
-    private formBuilder:FormBuilder){}
+    private formBuilder:FormBuilder,
+    private toastrService:ToastrService,
+    private authService:AuthService,
+    private router:Router){}
   
   ngOnInit(): void {
     this.getUser();
@@ -37,10 +43,38 @@ export class ProfileComponent implements OnInit {
     })
   }
 
+  changePassword(){
+    if(this.changePasswordForm.valid){
+      let changePasswordModel=Object.assign({},this.changePasswordForm.value);
+      
+      if(changePasswordModel.newPassword!=changePasswordModel.confirmNewPassword){
+        this.toastrService.error("Şifreler aynı değil","Hata");
+        return;
+      }
+
+      delete changePasswordModel["confirmNewPassword"];
+      changePasswordModel.email = this.user.email;
+
+      this.authService.changePassword(changePasswordModel).subscribe(response=>{
+        this.toastrService.success(response.message);
+        this.logOutAndGoLoginPage();
+      },errorResponse=>{
+        this.toastrService.error("Hatalı şifre","Hata!");
+      });
+    }
+    else{
+      this.toastrService.error("Geçersiz şifre bilgileri","Hata!");
+    }
+  }
+
+  logOutAndGoLoginPage(){
+    this.authService.logOut();
+    this.router.navigate(["login"]);
+  }
+
   getUser(){
     this.userService.getUserById(Number(localStorage.getItem("userId"))).subscribe(response=>{
       this.user=response.data;
     })
   }
-
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,6 +5,8 @@ import { Observable } from 'rxjs';
 import { TokenModel } from '../models/tokenModel';
 import { HttpClient } from '@angular/common/http';
 import { RegisterModel } from '../models/registerModel';
+import { ChangePasswordModel } from '../models/changePasswordModel';
+import { ResponseModel } from '../models/responseModel';
 
 @Injectable({
   providedIn: 'root'
@@ -22,6 +24,12 @@ export class AuthService {
     return this.httpClient.post<SingleResponseModel<TokenModel>>(this.apiUrl+"register",registerModel);
   }
 
+  logOut(){
+    localStorage.removeItem("token");
+    localStorage.removeItem("userId");
+    localStorage.removeItem("customerId");
+  }
+
   isAuthenticated(){
     if(localStorage.getItem("token")){
       return true;
@@ -30,4 +38,10 @@ export class AuthService {
       return false;
     }
   }
+
+  changePassword(changePasswordModel:ChangePasswordModel){
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"changepassword",changePasswordModel);
+  }
+
+
 }
